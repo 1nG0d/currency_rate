9541cabdac51e5646dafdb43e2daae137558fa8f
amartseniuk <martseniuk.anton@gmail.com>
2018-12-27 16:49:10 +0200

    Trow component reselect done

--- src/selectors/index.js
+++ src/selectors/index.js
@@ -5,46 +5,46 @@ export const quantitySelector = (state) => state.quantity
 
 // V1
 
-// export const exchangeDataSelector = (state) => state.exchangeData
-// export const dateSelector = (_,props) => props.date
-//
-// export const createCurrencyExchangeData = () =>{
-//     return createSelector(
-//         currencySelector,
-//         exchangeDataSelector,
-//         dateSelector,
-//         (currencyArray,exchangeData,date)=>{
-//             let result = [];
-//             if (exchangeData[date].length > 0 ){
-//                 const tmpArr = exchangeData[date].slice();
-//                 const obj = tmpArr.reduce(function(acc, cur, i) {
-//                     acc[cur.cc] = cur;
-//                     return acc;
-//                 }, {});
-//                 result = currencyArray.map((currency) => obj[currency].rate)
-//             }
-//             return result;
-//         })
-// }
+export const exchangeDataSelector = (state) => state.exchangeData
+export const dateSelector = (_,props) => props.date
+
+export const createCurrencyExchangeData = () =>{
+    return createSelector(
+        currencySelector,
+        exchangeDataSelector,
+        dateSelector,
+        (currencyArray,exchangeData,date)=>{
+            let result = [];
+            if (exchangeData[date].length > 0 ){
+                const tmpArr = exchangeData[date].slice();
+                const obj = tmpArr.reduce(function(acc, cur, i) {
+                    acc[cur.cc] = cur;
+                    return acc;
+                }, {});
+                result = currencyArray.map((currency) => obj[currency].rate)
+            }
+            return result;
+        })
+}
 
 
 // V2
-const currencyArraySelector = (state,props)=>{
-
-    const date = props.date
-    const exchangeData = state.exchangeData
-    const currencyArray = state.currency
-
-    if (exchangeData[date].length>0){
-     const obj = exchangeData[date].reduce(function(acc, cur, i) {
-         acc[cur.cc] = cur;
-         return acc;
-     }, {});
-        const result = currencyArray.map((currency) => obj[currency].rate)
-        return result
-    }
-}
-export const createCurrencyExchangeData = () => createSelector(
-    currencyArraySelector,
-    (resData) => resData
-)
+// const currencyArraySelector = (state,props)=>{
+//
+//     const date = props.date
+//     const exchangeData = state.exchangeData
+//     const currencyArray = state.currency
+//
+//     if (exchangeData[date].length>0){
+//      const obj = exchangeData[date].reduce(function(acc, cur, i) {
+//          acc[cur.cc] = cur;
+//          return acc;
+//      }, {});
+//         const result = currencyArray.map((currency) => obj[currency].rate)
+//         return result
+//     }
+// }
+// export const createCurrencyExchangeData = () => createSelector(
+//     currencyArraySelector,
+//     (resData) => resData
+// )
